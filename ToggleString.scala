c51684499afd97a3c5c6c4ec3ade63799112a9b2
Akhil1857 <akhil.trivedi@knoldus.com>
2023-03-09 15:11:56 +0530

    Updated using Try and catch block

--- ToggleString.scala
+++ ToggleString.scala
@@ -1,14 +1,28 @@
+import java.util.InputMismatchException
+import scala.io.StdIn.readLine
+
 object ToggleString extends App {
-  private val givenString: String = "KnolDus"
-  private var requiredString = ""
-  var index = 0
-  for (index <- 0 until givenString.length) {
-    if (Character.isLowerCase(givenString.charAt(index))) {
-      requiredString += Character.toUpperCase(givenString.charAt(index))
+  def stringConversion(givenString: String): String = {
+    var index = 0
+    var requiredString = ""
+    for (index <- 0 until givenString.length) {
+      if (Character.isLowerCase(givenString.charAt(index))) {
+        requiredString += Character.toUpperCase(givenString.charAt(index))
+      }
+      else {
+        requiredString += Character.toLowerCase(givenString.charAt(index))
+      }
     }
-    else {
-      requiredString += Character.toLowerCase(givenString.charAt(index))
-    }
+    return requiredString
   }
-  print(requiredString)
+
+  val givenString: String = readLine("Enter the String: ")
+  try {
+    if (givenString.isEmpty) {
+      throw new NullPointerException()
+    }
+  } catch {
+    case e: NullPointerException => println("Please enter the valid String it can not be "+e.getMessage)
+  }
+  print(stringConversion(givenString))
 }
